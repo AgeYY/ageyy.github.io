2c8dcdf4f44cf0e5724c19a076fc396812f88566
AgeYY <42469123+AgeYY@users.noreply.github.com>
2026-09-04 20:46:56 +0000

    Deploying to gh-pages from @ AgeYY/ageyy.github.io@e81f78ff37f29c25c113a5075ab6807075ce3942 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,4 +9,13 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },];
+  },{id: "news-our-paper-speed-modulations-in-grid-cell-information-geometry-was-published-in-nature-communications",
+          title: 'Our paper, “Speed modulations in grid cell information geometry”, was published in Nature...',
+          description: "",
+          section: "News",},{id: "news-successfully-defended-my-ph-d-in-physics-at-washington-university-in-st-louis-advised-by-prof-ralf-wessel",
+          title: 'Successfully defended my Ph.D. in Physics at Washington University in St. Louis, advised...',
+          description: "",
+          section: "News",},{id: "news-started-as-a-postdoctoral-fellow-in-the-department-of-neuroscience-at-the-university-of-texas-at-austin-working-with-prof-xue-xin-wei",
+          title: 'Started as a Postdoctoral Fellow in the Department of Neuroscience at The University...',
+          description: "",
+          section: "News",},];
